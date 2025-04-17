84dc8a309a096f93ed4dcd40160191199d5589fe
JRV-XVI <jrv.software@outlook.com>
2025-04-17 13:27:19 -0600

    alter init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-psql -U postgres -c "create database connect_the_schools;"
-psql -U postgres -c "create user admin with encrypted password 'admin';"
-psql -U postgres -c "grant all privileges on database connect_the_schools to admin;"
-psql -U postgres -d connect_the_schools -c "grant create on schema public to admin;"
-psql -U postgres -d connect_the_schools -c "grant usage on schema public to admin;"
+psql -U postgres -h localhost -c "create database connect_the_schools;"
+psql -U postgres -h localhost -c "create user admin with encrypted password 'admin';"
+psql -U postgres -h localhost -c "grant all privileges on database connect_the_schools to admin;"
+psql -U postgres -h localhost -d connect_the_schools -c "grant create on schema public to admin;"
+psql -U postgres -h localhost -d connect_the_schools -c "grant usage on schema public to admin;"
